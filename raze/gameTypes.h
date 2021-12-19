b9cbbe4f0bc62674788eae70e38496f38cc47d90
Emile Belanger <emile.belanger@gmail.com>
2021-12-19 15:56:33 +0000

    Created gameTypes.h file for Zeta touch

---
+++ raze/gameTypes.h
@@ -0,0 +1,8 @@
+#define RAZE_GAME_DUKE   100
+#define RAZE_GAME_BLOOD  101
+#define RAZE_GAME_SW     102
+#define RAZE_GAME_RR     103
+#define RAZE_GAME_NAM    104
+#define RAZE_GAME_PS     105
+#define RAZE_GAME_IONFURY  106
+#define RAZE_GAME_EDUKE32 107
--- raze/touch_interface.cpp
+++ raze/touch_interface.cpp
@@ -5,14 +5,8 @@
 #include "touch_interface.h"
 #include "SDL_keycode.h"
 
+#include "gameTypes.h"
 
-#define RAZE_GAME_DUKE   100
-#define RAZE_GAME_BLOOD  101
-#define RAZE_GAME_SW     102
-#define RAZE_GAME_RR     103
-#define RAZE_GAME_NAM    104
-#define RAZE_GAME_PS     105
-#define RAZE_GAME_IONFURY  106
 
 void TouchInterface::openGLStart()
 {
