4faed99d2d06f645bed7662d39dfa4b35d156273
Emile Belanger <emile.belanger@gmail.com>
2022-01-12 22:14:02 +0000

    Removed copy of old settings file for Delta

--- idtech1/touch_interface.cpp
+++ idtech1/touch_interface.cpp
@@ -4,8 +4,6 @@
 
 #include "touch_interface.h"
 #include "SDL_keycode.h"
-#include <fstream>
-
 
 #define GAME_TYPE_DOOM     1 // Dont use 0 so we can detect serialization
 #define GAME_TYPE_HEXEN    2
@@ -45,12 +43,6 @@ extern "C"
 #endif
 
 #endif
-}
-
-static bool is_file_exist(const char *fileName)
-{
-    std::ifstream infile(fileName);
-    return infile.good();
 }
 
 void TouchInterface::openGLStart()
@@ -421,19 +413,7 @@ void TouchInterface::createControlsDoom(std::string filesPath)
 	tcMouse->setAlpha(0.9);
 	tcMouse->signal_button.connect(sigc::mem_fun(this, &TouchInterface::mouseButton));
 
-	std::string oldSettings =  (std::string)filesPath +  "/touch_settings.xml";
 	std::string newSettings =  (std::string)filesPath +  "/touch_settings_" ENGINE_NAME ".xml";
-
-	// Copy old settings file
-	// Added 09/04/21 REMOVE AFTER SOME TIME
-    if(!is_file_exist(newSettings.c_str()))
-    {
-    	LOGI("COPYING OLD SETTINGS");
-		std::ifstream  src(oldSettings, std::ios::binary);
-		std::ofstream  dst(newSettings,   std::ios::binary);
-
-		dst << src.rdbuf();
-    }
 
 	UI_tc = touchcontrols::createDefaultSettingsUI(&controlsContainer, newSettings );
 	UI_tc->setAlpha(1);
