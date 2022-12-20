25dca69076d569e468166dc8a9adf2b8b644fc10
Emile Belanger <emile.belanger@gmail.com>
2022-12-20 15:40:23 +0000

    Added GZ only build option

--- Android.mk
+++ Android.mk
@@ -15,6 +15,8 @@ ifeq ($(G),D)
 LOCAL_STATIC_LIBRARIES := cert_chk
 else ifeq ($(G),Q)
 LOCAL_STATIC_LIBRARIES := cert_chk
+else ifeq ($(G),GZ)
+LOCAL_STATIC_LIBRARIES := cert_chk
 endif
 
 LOCAL_CFLAGS += -fvisibility=hidden -fdata-sections -ffunction-sections
