753053c29aa66e9adb7382edb95cc6eda5af3e23
Emile Belanger <emile.belanger@gmail.com>
2024-04-18 14:03:36 +0100

    Added rand.c() func export for arm 32

---
+++ rand.c
@@ -0,0 +1,11 @@
+//
+// Created by emile on 18/04/2024.
+//
+
+#ifdef __arm__
+//Before API 21 arm 32 does not expoert rand(), VPX library for GZDoom is built against API 21+ so need to define this here
+int rand(void)
+{
+    return (int)lrand48();
+}
+#endif
